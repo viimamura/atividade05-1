94ef04c2fa89b19d712608c2439c4a28bffc6199
vitoriasue <vitoria.imamura@hotmail.com>
2021-06-04 02:22:13 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,65 +1,63 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 app.use(express.json());
 app.use(cors());
 //app.listen(3000);
 
-
 app.get('/', function(req,res){res.send('Hello word')});
 
 
-const notes = [
+const users = [
       
 ]
 
-const endpoint = "/notes";
+const endpoint = "/users";
 
 app.get(endpoint, function(req,res){
-    res.send(notes.filter(Boolean));
+    res.send(users.filter(Boolean));
 });
 
 app.get(`${endpoint}/:id`, function(req, res){
         const id = req.params.id;
-        const note = notes[id];
+        const user = users[id];
 
-        if(!note){
+        if(!user){
             res.send("{}");
         }
         else{
-            res.send(note);
+            res.send(user);
         }
 });
 
 app.post(endpoint, function(req,res){
-    const note = {
-        id: notes.length,
+    const user = {
+        id: users.length,
         email: req.body["email"],
         senha: req.body["senha"]
     };
-    notes.push(note);
+    users.push(user);
     res.send("1");
     notify();
 
 });
 
 app.put(`${endpoint}/:id`, function(req,res){
-    const id =  parseInt( req.params.id);
-    const note = {
+    const id =  parseInt(req.params.id);
+    const user = {
         id: id,
         email: req.body["email"],
         senha: req.body["senha"]
     };
 
-    notes[id] = note;
+    users[id] = user;
     res.send("1");
     notify();
 })
 
 app.delete(`${endpoint}/:id`, function(req,res){
     const id = req.params.id;
-    delete notes[id];
+    delete users[id];
     res.send("1");
 
     notify();
